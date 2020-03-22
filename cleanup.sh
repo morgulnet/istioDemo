91244e900f943694df82a2c697a10cdc320c5d59
mo <mo@mc.local>
2020-03-22 21:59:08 +0300

    first

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl delete -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
+kubectl delete -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
+
+kubectl delete ns istio-system
+
+kubectl delete clusterrolebinding cluster-admin-binding
+
+kubectl delete -f ../auto-mtls/
+kubectl delete -f ../jwt
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# https://istio.io/docs/setup/install/istioctl/
+
+# Download Istio
+WORKDIR="`pwd`"
+ISTIO_VERSION="${ISTIO_VERSION:-1.5.0}"
+log "Downloading Istio ${ISTIO_VERSION}..."
+curl -L https://git.io/getLatestIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
+
+# Am I ready for install?
+istioctl verify-install
+
+# Prepare for install
+kubectl create namespace istio-system
+
+kubectl create clusterrolebinding cluster-admin-binding \
+    --clusterrole=cluster-admin \
+    --user=$(gcloud config get-value core/account)
+
+# Generate install manifest
+alias istioctl=${WORKDIR}/istio-${ISTIO_VERSION}/bin/istioctl
+
+istioctl manifest apply \
+--set values.grafana.enabled=true \
+--set values.kiali.enabled=true \
+--set values.tracing.enabled=true \
+--set values.kiali.enabled=true --set values.kiali.createDemoSecret=true \
+--set values.global.proxy.accessLogFile="/dev/stdout"
+
+
+# Deploy app
+kubectl label namespace default istio-injection=enabled
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/istio-manifests.yaml
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
+kubectl delete serviceentry whitelist-egress-google-metadata
+kubectl delete serviceentry whitelist-egress-googleapis
+kubectl delete svc frontend-external
